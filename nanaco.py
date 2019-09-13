945a419293c0d0cd9938db7bb6932c5bfb0e087b
finphie <19237170+finphie@users.noreply.github.com>
2019-09-13 22:57:13 +0900

    リファクタリング

--- nanaco.py
+++ nanaco.py
@@ -2,7 +2,6 @@ from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException
-import sys
 import time
 import argparse
 
@@ -14,65 +13,51 @@ parser.add_argument('gifts', nargs='+', help='ギフトID')
 
 args = parser.parse_args()
 
-driver = webdriver.Chrome('../chromedriver.exe')
+with webdriver.Chrome('../chromedriver.exe') as driver:
 
-# nanacoログインページに移動
-driver.get('https://www.nanaco-net.jp/pc/emServlet')
+    # nanacoログインページに移動
+    driver.get('https://www.nanaco-net.jp/pc/emServlet')
 
-# nanaco番号入力
-element = driver.find_elements_by_name('XCID')[1]
-element.send_keys(args.number)
+    # nanaco番号入力
+    driver.find_elements_by_name('XCID')[1].send_keys(args.number)
 
-# パスワード入力
-element = driver.find_element_by_name('LOGIN_PWD')
-element.send_keys(args.password)
+    # パスワード入力
+    driver.find_element_by_name('LOGIN_PWD').send_keys(args.password)
 
-# ログインボタンをクリック
-element = driver.find_element_by_name('ACT_ACBS_do_LOGIN1')
-element.click()
+    # ログインボタンをクリック
+    driver.find_element_by_name('ACT_ACBS_do_LOGIN1').click()
 
-# ギフトID登録画面に移動
-element = driver.find_element_by_css_selector('#gift > a')
-element.click()
+    # ギフトID登録画面に移動
+    driver.find_element_by_css_selector('#gift > a').click()
 
-for gift in args.gifts:
+    for gift in args.gifts:
 
-    # 登録ボタンをクリック
-    element = driver.find_element_by_xpath('//*[@id="register"]/form/p/input')
-    element.click()
+        # 登録ボタンをクリック
+        driver.find_element_by_xpath('//*[@id="register"]/form/p/input').click()
 
-    # ギフトID入力画面に遷移
-    WebDriverWait(driver, 15).until(EC.presence_of_all_elements_located)
-    gift_page_handle = driver.window_handles[1]
-    driver.switch_to.window(gift_page_handle)
+        # ギフトID入力画面に遷移
+        WebDriverWait(driver, 15).until(EC.presence_of_all_elements_located)
+        gift_page_handle = driver.window_handles[1]
+        driver.switch_to.window(gift_page_handle)
 
-    # ギフトID入力
-    print(gift, end='')
-    element = driver.find_element_by_id('gift01')
-    element.send_keys(gift[0:4])
-    element = driver.find_element_by_id('gift02')
-    element.send_keys(gift[4:8])
-    element = driver.find_element_by_id('gift03')
-    element.send_keys(gift[8:12])
-    element = driver.find_element_by_id('gift04')
-    element.send_keys(gift[12:16])
-    element = driver.find_element_by_id('submit-button')
-    element.click()
+        # ギフトID入力
+        print(gift, end='')
+        driver.find_element_by_id('gift01').send_keys(gift[0:4])
+        driver.find_element_by_id('gift02').send_keys(gift[4:8])
+        driver.find_element_by_id('gift03').send_keys(gift[8:12])
+        driver.find_element_by_id('gift04').send_keys(gift[12:16])
+        driver.find_element_by_id('submit-button').click()
 
-    # ギフトID登録
-    try:
-        element = driver.find_element_by_xpath('//*[@id="nav2Next"]/input[2]')
-        element.click()
-        print('...ok')
-    except NoSuchElementException:
-        print('...error')
+        # ギフトID登録
+        try:
+            driver.find_element_by_xpath('//*[@id="nav2Next"]/input[2]').click()
+            print('...ok')
+        except NoSuchElementException:
+            print('...error')
 
-    time.sleep(3)
-    driver.close()
-    driver.switch_to.window(driver.window_handles[0])
+        time.sleep(3)
+        driver.close()
+        driver.switch_to.window(driver.window_handles[0])
 
-# ログアウト
-element = driver.find_element_by_css_selector('#logout > a')
-element.click()
-
-driver.quit()
+    # ログアウト
+    driver.find_element_by_css_selector('#logout > a').click()
